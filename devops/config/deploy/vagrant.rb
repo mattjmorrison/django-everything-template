5e4445145baa6ad74cd63c6fcfc613e949d08414
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2013-01-27 00:45:11 -0600

    deleted comments

--- devops/config/deploy/vagrant.rb
+++ devops/config/deploy/vagrant.rb
@@ -12,12 +12,3 @@ set :deploy_to, "/var/www/django-everything/"
 
 # App Info
 set :django_env, 'production'
-
-# Need name of deployed wsgi file
-# Need name of deployed settings file
-# Need name of optional maintenance wsgi file
-# Need server restart command
-
-# Need flag to run syncdb (with and without migrations)
-# Need flag to run collect static (with -i *.coffee -i *.less)
-# Need flag to run compress
