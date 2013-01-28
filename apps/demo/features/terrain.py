697671f86d6138092a6fc906e54f19faffaaf612
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2013-01-27 23:29:07 -0600

    fixed flake8 violations

--- apps/demo/features/terrain.py
+++ apps/demo/features/terrain.py
@@ -4,14 +4,14 @@ from coverage import coverage
 
 @before.all
 def prepare_coverage():
-	world.cov = coverage(source=['apps'], omit=['*/features/*', '*/migrations/*', '*/tests/*'])
-	world.cov.start()
+    world.cov = coverage(source=['apps'], omit=['*/features/*', '*/migrations/*', '*/tests/*'])
+    world.cov.start()
 
 
 @after.all
-def teardown_browser(total):
-	world.cov.save()
-	coverage_percent = world.cov.report()
-	world.cov.stop()
-	if coverage_percent < 95.0:
-		raise Exception("TOTAL Coverage did not reach minimum required: 96%")
+def teardown_coverage(total):
+    world.cov.save()
+    coverage_percent = world.cov.report()
+    world.cov.stop()
+    if coverage_percent < 95.0:
+        raise Exception("TOTAL Coverage did not reach minimum required: 96%")
