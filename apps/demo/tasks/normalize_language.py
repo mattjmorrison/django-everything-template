2ecb6509f8f2b87f87c0547d24cc0b79da46dee5
Percy <toran.billups@theimtgroup.com>
2013-02-13 11:00:25 -0600

    added a periodic celery task

--- apps/demo/tasks/normalize_language.py
+++ apps/demo/tasks/normalize_language.py
@@ -1,6 +1,14 @@
 from celery import task
+from celery.schedules import crontab
+
 
 @task(name="add")
 def add(*args, **kwargs):
     print "Inside My Add Task with args: {} and kwargs: {}".format(args, kwargs)
+    return "This is the return value!"
+
+
+@task.periodic_task(name="tick", run_every=crontab(minute='*/1'))
+def tick(*args, **kwargs):
+    print "Inside my periodic task with args: {} and kwargs: {}".format(args, kwargs)
     return "This is the return value!"
